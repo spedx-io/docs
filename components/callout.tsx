51f76cad8a75f7dde178ab015cca3db4d4d7525c
Karthik Pariti <karthik@spedx.exchange>
2023-10-08 21:29:45 +0530

    feat: add introduction page

---
+++ components/callout.tsx
@@ -0,0 +1,7 @@
+import { Callout } from "nextra-theme-docs";
+
+export default function CalloutText() {
+    return (<Callout type="info" emoji="ℹ️">
+    ZDFs are the brainchilds of HxroNetwork. SpedX is a Hxro Ecosystem dApp and leverages its derivatives infrastructure to power ZDF trading.
+</Callout>)
+}
